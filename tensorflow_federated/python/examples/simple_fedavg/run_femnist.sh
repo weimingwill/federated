fe4f17258fee00f27e45ee5946688a9a2502f264
Weiming <wingalong@gmail.com>
2021-05-24 00:38:07 +0800

    feat[script]: update script

--- tensorflow_federated/python/examples/simple_fedavg/run_femnist.sh
+++ tensorflow_federated/python/examples/simple_fedavg/run_femnist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p log
+now=$(date +"%Y%m%d_%H%M%S")
 
 data_dir=~/personal-projects/easyfl/easyfl/datasets/data
 root_dir=~/personal-projects/federated/
@@ -10,4 +11,4 @@ export PYTHONPATH=$PYTHONPATH:${root_dir}
 python ${root_dir}/tensorflow_federated/python/examples/simple_fedavg/emnist_fedavg_main.py \
     --data_dir ${data_dir}/femnist/femnist_iid_100_10_1_0.05_0.1_sample_0.9/ \
     --total_rounds 150 --train_clients_per_round 10 --client_epochs_per_round 5 \
-    --batch_size 64 --test_batch_size 5 --client_learning_rate 0.01
+    --batch_size 64 --test_batch_size 5 --client_learning_rate 0.01 | tee log/tff-${now}.log &
