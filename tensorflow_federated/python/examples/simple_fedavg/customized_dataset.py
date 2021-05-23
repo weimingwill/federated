b0d2886b506a339bda386b8a8ef0a986413a7ad6
Weiming <wingalong@gmail.com>
2021-05-23 21:07:49 +0800

    feat[emnist]: customize dataset for femnist

---
+++ tensorflow_federated/python/examples/simple_fedavg/customized_dataset.py
@@ -0,0 +1,118 @@
+import collections
+import json
+import os
+from collections import defaultdict
+
+import tensorflow as tf
+
+from tensorflow_federated.python.simulation import client_data
+
+
+def load_data(data_dir, dataset):
+    if data_dir == "":
+        train_data_dir = os.path.join('..', 'data', dataset, 'data', 'train')
+        test_data_dir = os.path.join('..', 'data', dataset, 'data', 'test')
+    else:
+        train_data_dir = os.path.join(data_dir, "train")
+        test_data_dir = os.path.join(data_dir, "test")
+    users, groups, train_data, test_data = read_data(train_data_dir, test_data_dir)
+    return CustomizedDataset(train_data, users), CustomizedDataset(test_data, users)
+
+
+def read_data(train_data_dir, test_data_dir):
+    '''parses data in given train and test data directories
+
+    assumes:
+    - the data in the input directories are .json files with
+        keys 'users' and 'user_data'
+    - the set of train set users is the same as the set of test set users
+
+    Return:
+        clients: list of client ids
+        groups: list of group ids; empty list if none found
+        train_data: dictionary of train data
+        test_data: dictionary of test data
+    '''
+    train_clients, train_groups, train_data = read_dir(train_data_dir)
+    test_clients, test_groups, test_data = read_dir(test_data_dir)
+
+    assert train_clients == test_clients
+    assert train_groups == test_groups
+
+    return train_clients, train_groups, train_data, test_data
+
+
+def read_dir(data_dir):
+    clients = []
+    groups = []
+    data = defaultdict(lambda: None)
+
+    files = os.listdir(data_dir)
+    files = [f for f in files if f.endswith('.json')]
+    for f in files:
+        file_path = os.path.join(data_dir, f)
+        with open(file_path, 'r') as inf:
+            cdata = json.load(inf)
+        clients.extend(cdata['users'])
+        if 'hierarchies' in cdata:
+            groups.extend(cdata['hierarchies'])
+        data.update(cdata['user_data'])
+
+    clients = list(sorted(data.keys()))
+    return clients, groups, data
+
+
+class CustomizedDataset(client_data.ClientData):
+    def __init__(self, data, clients):
+        self._data = data
+        self._client_ids = clients
+        self._element_type_structure = tf.TensorSpec(dtype=tf.string, shape=())
+
+    def _create_dataset(self, client_id):
+        """Creates a `tf.data.Dataset` for a client in a TF-serializable manner."""
+
+        dic = collections.OrderedDict()
+        for name, ds in self._data[client_id].items():
+            if name == 'x':
+                dic['pixels'] = tf.reshape(ds, [-1, 28, 28])
+
+            if name == 'y':
+                dic['label'] = ds[:]
+
+        return tf.data.Dataset.from_tensor_slices(dic)
+
+    @property
+    def serializable_dataset_fn(self):
+        return self._create_dataset
+
+    @property
+    def client_ids(self):
+        return self._client_ids
+
+    def create_tf_dataset_for_client(self, client_id: str):
+        """Creates a new `tf.data.Dataset` containing the client training examples.
+
+        This function will create a dataset for a given client if `client_id` is
+        contained in the `client_ids` property of the `SQLClientData`. Unlike
+        `self.serializable_dataset_fn`, this method is not serializable.
+
+        Args:
+          client_id: The string identifier for the desired client.
+
+        Returns:
+          A `tf.data.Dataset` object.
+        """
+        if client_id not in self.client_ids:
+            raise ValueError(
+                "ID [{i}] is not a client in this ClientData. See "
+                "property `client_ids` for the list of valid ids.".format(
+                    i=client_id))
+        return self._create_dataset(client_id)
+
+    @property
+    def element_type_structure(self):
+        return self._element_type_structure
+
+    @property
+    def dataset_computation(self):
+        raise NotImplementedError("b/162106885")
--- tensorflow_federated/python/examples/simple_fedavg/emnist_fedavg_main.py
+++ tensorflow_federated/python/examples/simple_fedavg/emnist_fedavg_main.py
@@ -16,7 +16,7 @@
 This is intended to be a minimal stand-alone experiment script built on top of
 core TFF.
 """
-
+import time
 import collections
 import functools
 from absl import app
@@ -27,6 +27,7 @@ import tensorflow_federated as tff
 
 from tensorflow_federated.python.examples.simple_fedavg import simple_fedavg_tf
 from tensorflow_federated.python.examples.simple_fedavg import simple_fedavg_tff
+from tensorflow_federated.python.examples.simple_fedavg.customized_dataset import load_data
 
 # Training hyperparameters
 flags.DEFINE_integer('total_rounds', 256, 'Number of total training rounds.')
@@ -35,8 +36,9 @@ flags.DEFINE_integer('train_clients_per_round', 2,
                      'How many clients to sample per round.')
 flags.DEFINE_integer('client_epochs_per_round', 1,
                      'Number of epochs in the client to take per round.')
-flags.DEFINE_integer('batch_size', 20, 'Batch size used on the client.')
-flags.DEFINE_integer('test_batch_size', 100, 'Minibatch size of test data.')
+flags.DEFINE_integer('batch_size', 16, 'Batch size used on the client.')
+flags.DEFINE_integer('test_batch_size', 128, 'Minibatch size of test data.')
+flags.DEFINE_string('data_dir', "", 'customized data directory')
 
 # Optimizer configuration (this defines one or more flags per optimizer).
 flags.DEFINE_float('server_learning_rate', 1.0, 'Server learning rate.')
@@ -45,17 +47,48 @@ flags.DEFINE_float('client_learning_rate', 0.1, 'Client learning rate.')
 FLAGS = flags.FLAGS
 
 
+def get_emnist_dataset_new(data_dir):
+  """Loads and preprocesses the EMNIST dataset.
+
+  Returns:
+    A `(emnist_train, emnist_test)` tuple where `emnist_train` is a
+    `tff.simulation.datasets.ClientData` object representing the training data
+    and `emnist_test` is a single `tf.data.Dataset` representing the test data
+    of all clients.
+  """
+  emnist_train, emnist_test = load_data(data_dir, "femnist")
+
+  def element_fn(element):
+    return collections.OrderedDict(
+        x=tf.expand_dims(element['pixels'], -1), y=element['label'])
+
+  def preprocess_train_dataset(dataset):
+    # Use buffer_size same as the maximum client dataset size,
+    # 418 for Federated EMNIST
+    return dataset.map(element_fn).shuffle(buffer_size=418).repeat(
+        count=FLAGS.client_epochs_per_round).batch(
+            FLAGS.batch_size, drop_remainder=False)
+
+  def preprocess_test_dataset(dataset):
+    return dataset.map(element_fn).batch(
+        FLAGS.test_batch_size, drop_remainder=False)
+
+  emnist_train = emnist_train.preprocess(preprocess_train_dataset)
+  emnist_test = preprocess_test_dataset(
+      emnist_test.create_tf_dataset_from_all_clients())
+  return emnist_train, emnist_test
+
+
 def get_emnist_dataset():
   """Loads and preprocesses the EMNIST dataset.
 
   Returns:
     A `(emnist_train, emnist_test)` tuple where `emnist_train` is a
-    `tff.simulation.ClientData` object representing the training data and
-    `emnist_test` is a single `tf.data.Dataset` representing the test data of
-    all clients.
+    `tff.simulation.datasets.ClientData` object representing the training data
+    and `emnist_test` is a single `tf.data.Dataset` representing the test data
+    of all clients.
   """
-  emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data(
-      only_digits=True)
+  emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data(only_digits=False)
 
   def element_fn(element):
     return collections.OrderedDict(
@@ -80,9 +113,6 @@ def get_emnist_dataset():
 
 def create_original_fedavg_cnn_model(only_digits=True):
   """The CNN model used in https://arxiv.org/abs/1602.05629.
-
-  This function is duplicated from research/optimization/emnist/models.py to
-  make this example completely stand-alone.
 
   Args:
     only_digits: If True, uses a final layer with 10 outputs, for use with the
@@ -115,7 +145,6 @@ def create_original_fedavg_cnn_model(only_digits=True):
       tf.keras.layers.Flatten(),
       tf.keras.layers.Dense(512, activation=tf.nn.relu),
       tf.keras.layers.Dense(10 if only_digits else 62),
-      tf.keras.layers.Activation(tf.nn.softmax),
   ])
 
   return model
@@ -126,21 +155,38 @@ def server_optimizer_fn():
 
 
 def client_optimizer_fn():
-  return tf.keras.optimizers.SGD(learning_rate=FLAGS.client_learning_rate)
+  return tf.keras.optimizers.SGD(learning_rate=FLAGS.client_learning_rate, momentum=0.9)
 
 
 def main(argv):
+  start_time = time.time()
+  print("args", argv)
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
 
-  train_data, test_data = get_emnist_dataset()
+  # If GPU is provided, TFF will by default use the first GPU like TF. The
+  # following lines will configure TFF to use multi-GPUs and distribute client
+  # computation on the GPUs. Note that we put server computatoin on CPU to avoid
+  # potential out of memory issue when a large number of clients is sampled per
+  # round. The client devices below can be an empty list when no GPU could be
+  # detected by TF.
+  client_devices = tf.config.list_logical_devices('GPU')
+  server_device = tf.config.list_logical_devices('CPU')[0]
+  tff.backends.native.set_local_execution_context(
+      server_tf_device=server_device, client_tf_devices=client_devices)
+
+  # train_data, test_data = get_emnist_dataset()
+  train_data, test_data = get_emnist_dataset_new(FLAGS.data_dir)
 
   def tff_model_fn():
     """Constructs a fully initialized model for use in federated averaging."""
-    keras_model = create_original_fedavg_cnn_model(only_digits=True)
-    loss = tf.keras.losses.SparseCategoricalCrossentropy()
-    return simple_fedavg_tf.KerasModelWrapper(keras_model,
-                                              test_data.element_spec, loss)
+    keras_model = create_original_fedavg_cnn_model(only_digits=False)
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    element_spec = collections.OrderedDict(
+        x=tf.TensorSpec([None, 28, 28, 1], tf.float32),
+        y=tf.TensorSpec([None], tf.int32))
+    return simple_fedavg_tf.KerasModelWrapper(keras_model, element_spec, loss)
+    # return simple_fedavg_tf.KerasModelWrapper(keras_model, test_data.element_spec, loss)
 
   iterative_process = simple_fedavg_tff.build_federated_averaging_process(
       tff_model_fn, server_optimizer_fn, client_optimizer_fn)
@@ -148,6 +194,9 @@ def main(argv):
 
   metric = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
   model = tff_model_fn()
+
+  cumulative_accuracies = []
+  cumulative_training_times = []
   for round_num in range(FLAGS.total_rounds):
     sampled_clients = np.random.choice(
         train_data.client_ids,
@@ -162,9 +211,13 @@ def main(argv):
     print(f'Round {round_num} training loss: {train_metrics}')
     if round_num % FLAGS.rounds_per_eval == 0:
       model.from_weights(server_state.model_weights)
-      accuracy = simple_fedavg_tf.keras_evaluate(model.keras_model, test_data,
-                                                 metric)
+      accuracy = simple_fedavg_tf.keras_evaluate(model.keras_model, test_data, metric)
       print(f'Round {round_num} validation accuracy: {accuracy * 100.0}')
+      cumulative_accuracies.append(accuracy.numpy() * 100.0)
+      cumulative_training_times.append(time.time() - start_time)
+  print("Cumulative accuracies:", cumulative_accuracies)
+  print("Cumulative training times:", cumulative_training_times)
+  print("Total training time:", time.time() - start_time)
 
 
 if __name__ == '__main__':
