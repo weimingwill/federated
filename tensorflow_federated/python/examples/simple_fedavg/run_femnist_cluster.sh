1c23e3681ebc3c29313e27611bac2522f512b52e
Weiming <wingalong@gmail.com>
2021-06-02 00:54:29 +0800

    update script

--- tensorflow_federated/python/examples/simple_fedavg/run_femnist_cluster.sh
+++ tensorflow_federated/python/examples/simple_fedavg/run_femnist_cluster.sh
@@ -13,5 +13,5 @@ srun -u --partition=innova --job-name=femnist \
     python ${root_dir}/tensorflow_federated/python/examples/simple_fedavg/fedavg_main.py \
         --data_dir ${data_dir}/femnist/femnist_iid_10_10_1_0.05_0.05_sample_0.9/ --dataset femnist \
         --total_rounds 150 --train_clients_per_round 10 --client_epochs_per_round 10 \
-        --batch_size 64 --test_batch_size 64 --client_learning_rate 0.01 --test_all 2>&1 | tee log/tff-${now}.log &
+        --batch_size 64 --test_batch_size 64 --client_learning_rate 0.01 --test_all 2>&1 | tee log/tff-femnist-${now}.log &
 
