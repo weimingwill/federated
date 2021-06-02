ca0880b3f23649f58a91917e856e9012f0f5443b
Weiming <wingalong@gmail.com>
2021-06-02 17:14:08 +0800

    update script

--- tensorflow_federated/python/examples/simple_fedavg/fedavg_main.py
+++ tensorflow_federated/python/examples/simple_fedavg/fedavg_main.py
@@ -49,6 +49,7 @@ flags.DEFINE_string('data_dir', "", 'customized data directory')
 flags.DEFINE_string('dataset', "", 'dataset name')
 flags.DEFINE_boolean('test_all', False, 'test all clients')
 flags.DEFINE_boolean('test_in_server', False, 'test in centralized server')
+flags.DEFINE_boolean('use_cpu', False, 'whether use gpu for training')
 
 # Optimizer configuration (this defines one or more flags per optimizer).
 flags.DEFINE_float('server_learning_rate', 1.0, 'Server learning rate.')
@@ -188,8 +189,13 @@ def main(argv):
     # potential out of memory issue when a large number of clients is sampled per
     # round. The client devices below can be an empty list when no GPU could be
     # detected by TF.
-    client_devices = tf.config.list_logical_devices('GPU')
-    server_device = tf.config.list_logical_devices('CPU')[0]
+    if FLAGS.use_cpu:
+        client_devices = tf.config.list_logical_devices('CPU')
+        server_device = tf.config.list_logical_devices('CPU')[0]
+    else:
+        client_devices = tf.config.list_logical_devices('GPU')
+        server_device = tf.config.list_logical_devices('CPU')[0]
+
     tff.backends.native.set_local_execution_context(
         server_tf_device=server_device, client_tf_devices=client_devices)
 
--- tensorflow_federated/python/examples/simple_fedavg/run_shakespeare_cluster_8gpu.sh
+++ tensorflow_federated/python/examples/simple_fedavg/run_shakespeare_cluster_8gpu.sh
@@ -13,4 +13,4 @@ srun -u --partition=innova --job-name=shakespeare8 \
     python -u ${root_dir}/tensorflow_federated/python/examples/simple_fedavg/fedavg_main.py \
         --data_dir ${data_dir}/shakespeare/shakespeare_iid_10_10_1_0.2_0.2_sample_0.9/ \
         --dataset shakespeare --total_rounds 100 --train_clients_per_round 10 --client_epochs_per_round 10 \
-        --batch_size 64 --test_batch_size 64 --client_learning_rate 0.8 | tee log/tff-shakespeare-${now}.log &
+        --batch_size 64 --test_batch_size 64 --client_learning_rate 0.8 --test_all | tee log/tff-shakespeare-${now}.log &
