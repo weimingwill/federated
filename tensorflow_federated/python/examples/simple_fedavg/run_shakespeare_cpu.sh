c7e5b79675dbe81629f72c239f33e02e40c203ea
Weiming <wingalong@gmail.com>
2021-06-03 20:59:54 +0800

    update script

--- tensorflow_federated/python/examples/simple_fedavg/run_shakespeare_cpu.sh
+++ tensorflow_federated/python/examples/simple_fedavg/run_shakespeare_cpu.sh
@@ -10,5 +10,5 @@ export PYTHONPATH=$PYTHONPATH:${root_dir}
 
 python -u ${root_dir}/tensorflow_federated/python/examples/simple_fedavg/fedavg_main.py \
     --data_dir ${data_dir}/shakespeare/shakespeare_iid_10_10_1_0.2_0.2_sample_0.9/ \
-    --dataset shakespeare --total_rounds 1 --train_clients_per_round 1 --client_epochs_per_round 1 \
+    --dataset shakespeare --total_rounds 100 --train_clients_per_round 10 --client_epochs_per_round 10 \
     --batch_size 64 --test_batch_size 64 --client_learning_rate 0.8 --test_all  --gpu 0 | tee log/tff-shakespeare-${now}.log &
